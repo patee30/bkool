aa515c8b5f4369e8b7d65724385439642b721469
patee30 <tientrantan30@gmail.com>
2021-10-27 23:10:13 +0700

    Working

--- main/bkool/checker/StaticCheck.py
+++ main/bkool/checker/StaticCheck.py
@@ -17,10 +17,41 @@ class MType:
 
 
 class Symbol:
-    def __init__(self, name, mtype, value=None):
+    def __init__(self, name, mtype, value=None, details=None):
         self.name = name
         self.mtype = mtype
         self.value = value
+        self.details = details
+
+
+class Env:
+    def __init__(self, globalEnv={}, localEnv=None, currentEnv=None):
+        self.o = {}
+        self.o['global'] = globalEnv
+        self.o['local'] = localEnv
+        self.o['current'] = currentEnv
+
+    def openScope(self, o):
+        o['local'].append({})
+
+    def closeScope(self, o):
+        o['local'].pop()
+
+    def lookUp(self, name):
+        if len(self.o['local']):
+            if name in self.o['local'][-1]:
+                return True
+            return False
+        else:
+            pass
+
+
+class Sup:
+    def lookUp(self):
+        pass
+
+    def lookUpLocal(self):
+        pass
 
 
 class GetEnv(BaseVisitor):
@@ -39,8 +70,11 @@ class GetEnv(BaseVisitor):
         # parentname : Id = None # None if there is no parent
         if ast.classname.name in o:
             raise Redeclared(Class(), ast.classname.name)
-        o[ast.classname.name] = {}
-        [self.visit(x, o[ast.classname.name]) for x in ast.memlist]
+        o[ast.classname.name] = {
+            'parent': ast.parentname.name if ast.parentname else None,
+            'members': {}
+        }
+        [self.visit(x, o[ast.classname.name]['members']) for x in ast.memlist]
 
     def visitMethodDecl(self, ast, o):
         # kind: SIKind
@@ -108,16 +142,15 @@ class StaticChecker(BaseVisitor):
         return self.visit(self.ast, StaticChecker.global_envi)
 
     def visitProgram(self, ast, o):
-        env = GetEnv().visit(ast, None)
+        env = GetEnv().visit(ast, Env())
         return [self.visit(x, env) for x in ast.decl]
 
     def visitClassDecl(self, ast, o):
         env = {}
-        env['current'] = ast.classname.name
         env['global'] = o
+        env['current'] = ast.classname.name
         for mem in ast.memlist:
-            if type(mem) is MethodDecl:
-                self.visit(mem, env)
+            self.visit(mem, env)
 
     def visitMethodDecl(self, ast, o):
         # kind: SIKind
@@ -126,16 +159,53 @@ class StaticChecker(BaseVisitor):
         # returnType: Type  # None for constructor
         # body: Block
         env = {}
-        env['current'] = o['current']
         env['global'] = o['global']
+        env['current'] = o['current']
         env['local'] = {}
-        [GetEnv().visit(parameter, env['local']) for parameter in ast.param]
-        self.visit(ast.body, env['local'])
+        [self.visit(parameter, env['local']) for parameter in ast.param]
+        self.visit(ast.body, env)
+
+    def visitAttributeDecl(self, ast, o):
+        # kind: SIKind #Instance or Static
+        # decl: StoreDecl # VarDecl for mutable or ConstDecl for immutable
+        if type(ast.decl) is VarDecl:
+            if type(ast.decl.varType) is ClassType:
+                if not ast.decl.varType.classname.name in o['global']:
+                    raise Undeclared(Class(), ast.decl.varType.classname.name)
+
+        else:
+            if type(ast.decl.constType) is ClassType:
+                if not ast.decl.varType.classname.name in o['global']:
+                    raise Undeclared(Class(), ast.decl.varType.classname.name)
+            if type(ast.decl.constType) is IntType:
+                if not self.visit(ast.decl.value, o).mtype is IntType: raise TypeMismatchInConstant(ast.decl)
+            elif type(ast.decl.constType) is FloatType:
+                if not self.visit(ast.decl.value, o).mtype in [IntType, FloatType]: raise TypeMismatchInConstant(ast.decl)
+            elif type(ast.decl.constDecl) is BoolType:
+                if not self.visit(ast.decl.value, o).mtype  is BoolType: raise TypeMismatchInConstant(ast.decl)
+
+    def visitConstDecl(self, ast, o):
+        # constant : Id
+        # constType : Type
+        # value : Expr
+        if ast.constant.name in o:
+            raise Redeclared(Constant(), ast.constant.name)
+        o[ast.variable.name] = ast.constType
+
+    def visitVarDecl(self, ast, o):
+        # variable : Id
+        # varType : Type
+        # varInit : Expr = None # None if there is no initial
+        if ast.variable.name in o:
+            raise Redeclared(Variable(), ast.variable.name)
+        o[ast.variable.name] = ast.varType
 
     def visitBlock(self, ast, o):
         # decl:List[StoreDecl]
         # stmt:List[Stmt]
-        [GetEnv().visit(decl, o) for decl in ast.decl]
+        [self.visit(decl, o['local']) for decl in ast.decl]
+
+        [self.visit(stmt, o) for stmt in ast.stmt]
 
     def visitBinaryOp(self, ast, o):
         # op:str
@@ -143,29 +213,91 @@ class StaticChecker(BaseVisitor):
         # right:Expr
         l = self.visit(ast.left, o)
         r = self.visit(ast.right, o)
-        print(o)
-        #Arithmetic
+        if l is None:
+            raise Undeclared(Identifier(), ast.left.name)
+        # Arithmetic
         if ast.op in ['+', '-', '*', '/', '\\', '%']:
-            print('Alo')
+            if not l.mtype and r.mtype in [IntType, FloatType]:
+                raise TypeMismatchInExpression(ast)
+            elif type(l.mtype) != type(r.mtype) and l.mtype != IntType and (ast.op in ['\\', '%']):
+                raise TypeMismatchInExpression(ast)
+            elif ast.op == '/':
+                return Symbol('', FloatType)
+            elif type(l.mtype) != type(r.mtype):
+                return Symbol('', FloatType)
+            else:
+                return l
 
+        # Boolean
+        elif ast.op in ['&&', '||']:
+            if not l.mtype and r.mtype == BoolType:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return Symbol('', BoolType)
+
+        # Relational
+        elif ast.op in ['==', '!=', '>', '<', '>=', '<=']:
+            if ast.op in ['==', '!=']:
+                if (not l.mtype and r.mtype in [IntType, BoolType]) or type(l.mtype) != type(r.mtype):
+                    raise TypeMismatchInExpression(ast)
+            else:
+                if (not l.mtype and r.mtype in [IntType, FloatType]):
+                    raise TypeMismatchInExpression(ast)
+
+            return Symbol('', BoolType)
+
+        # String
+        elif ast.op == '^':
+            if l.mtype and r.mtype != StringType:
+                raise TypeMismatchInExpression(ast)
+            return Symbol('', BoolType)
+
+    def visitUnaryOp(self, ast, o):
+        # op:str
+        # body:Expr
+        body = self.visit(ast.body, o)
+
+        if ast.op in ['+', '-']:
+            if not body.mtype in [IntType, FloatType]:
+                raise TypeMismatchInExpression(ast)
+            else:
+                return body.mtype
+
+        elif ast.op == '!':
+            if body.mtype != BoolType:
+                raise TypeMismatchInExpression(ast)
+
+    def visitAssign(self, ast, o):
+        # lhs:Expr
+        # exp:Expr
+        lhs = self.visit(ast.lhs, o)
+        exp = self.visit(ast.exp, o)
+        if type(lhs) != type(exp.mtype):
+            raise TypeMismatchInStatement(ast)
+    
     def visitId(self, ast, o):
-        if ast.name in o:
-            return o[ast.name]
+        print(o['local'])
+        if ast.name in o['local']:
+            return o['local'][ast.name]
+ 
+        if ast.name in o['global'][o['current']]['members']:
+            return o['global'][o['current']]['members'][ast.name]
+            
         raise Undeclared(Identifier(), ast.name)
 
     def visitIntLiteral(self, ast, o):
-        return 'Int'
+        return Symbol('', IntType())
 
     def visitFloatLiteral(self, ast, o):
-        return 'Float'
+        return Symbol('', FloatType())
 
     def visitBooleanLiteral(self, ast, o):
-        return 'Boolean'
+        return Symbol('', BoolType())
 
     def visitStringLiteral(self, ast, o):
-        return 'String'
+        return Symbol('', StringType())
 
     def visitNullLiteral(self, ast, o):
-        return 'Null'
+        pass
 
     # def visitSelfLiteral(self, ast, param):
--- test/CheckerSuite.py
+++ test/CheckerSuite.py
@@ -4,54 +4,86 @@ from AST import *
 
 
 class CheckerSuite(unittest.TestCase):
-    # def test_00(self):
-    #     input = """class main {}
-    #     class main {}"""
-    #     expect = "Redeclared Class: main"
-    #     self.assertTrue(TestChecker.test(input, expect, 400))
+    def test_00(self):
+        input = """class main {}
+        class main {}"""
+        expect = "Redeclared Class: main"
+        self.assertTrue(TestChecker.test(input, expect, 400))
 
-    # def test_01(self):
-    #     input = """class main {
-    #         int a, a;
-    #     }"""
-    #     expect = "Redeclared Attribute: a"
-    #     self.assertTrue(TestChecker.test(input,expect,401))
+    def test_01(self):
+        input = """class main {
+            int a, a;
+        }"""
+        expect = "Redeclared Attribute: a"
+        self.assertTrue(TestChecker.test(input,expect,401))
 
-    # def test_02(self):
-    #     input = """class main {
-    #         void a() {}
-    #         int a() {}
-    #     }"""
-    #     expect = "Redeclared Method: a"
-    #     self.assertTrue(TestChecker.test(input,expect,402))
+    def test_02(self):
+        input = """class main {
+            void a() {}
+            int a() {}
+        }"""
+        expect = "Redeclared Method: a"
+        self.assertTrue(TestChecker.test(input,expect,402))
 
-    # def test_03(self):
-    #     input = """class main {
-    #         static int a;
-    #         static int a;
-    #     }"""
-    #     expect = "Redeclared Attribute: a"
-    #     self.assertTrue(TestChecker.test(input,expect,403))
+    def test_03(self):
+        input = """class main {
+            static int a;
+            static int a;
+        }"""
+        expect = "Redeclared Attribute: a"
+        self.assertTrue(TestChecker.test(input,expect,403))
 
-    # def test_04(self):
-    #     input = """class main {
-    #         void a () {
-    #             int a;
-    #             int a;
-    #         }
-    #     }
-    #     """
-    #     expect = "Redeclared Variable: a"
-    #     self.assertTrue(TestChecker.test(input,expect,404))
-
-    def test_05(self):
+    def test_04(self):
         input = """class main {
             void a () {
-                int b;
-                int a = b + 5;
+                int a;
+                int a;
             }
         }
         """
         expect = "Redeclared Variable: a"
+        self.assertTrue(TestChecker.test(input,expect,404))
+
+    def test_05(self):
+        input = """class main {
+            static A t;
+            static void x () {
+                int a;
+            }
+        }
+        class abc {
+            abc x;
+        }
+        """
+        expect = "Undeclared Class: A"
         self.assertTrue(TestChecker.test(input,expect,405))
+
+    def test_06(self):
+        input = """class main {
+            final int a = 1.0 % 2;
+        }
+        """
+        expect = "Type Mismatch In Expression: BinaryOp(%,FloatLit(1.0),IntLit(2))"
+        self.assertTrue(TestChecker.test(input,expect,406))
     
+    def test_07(self):
+        input = """class main {
+            int a () {
+                int t;
+                t := 0.5;
+            }
+        }
+        """
+        expect = "Type Mismatch In Statement: AssignStmt(Id(t),FloatLit(0.5))"
+        self.assertTrue(TestChecker.test(input,expect,407))
+
+    def test_08(self):
+        input = """class main {
+            int t;
+            int a () {
+                t := 1.0 + 2;
+            }
+        }
+        """
+        expect = "Type Mismatch In Statement: AssignStmt(Id(t),BinaryOp(+,FloatLit(1.0),IntLit(2)))"
+        self.assertTrue(TestChecker.test(input,expect,408))
